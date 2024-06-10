2680b8a4ed2c1f3bf54739f99c6011015199823a
student <student@AQ2-0673>
2024-06-10 14:04:44 +0900

    6/10entity

--- src/main/java/com/example/demo/entity/Account.java
+++ src/main/java/com/example/demo/entity/Account.java
@@ -35,4 +35,58 @@ public class Account {
 	public Account() {
 	}
 
+	public Account(String userName, String nickname, String email, String password, Integer privilege, Boolean ban,
+			Boolean deleted) {
+		this.userName = userName;
+		this.nickname = nickname;
+		this.email = email;
+		this.password = password;
+		this.privilege = privilege;
+		this.ban = ban;
+		this.deleted = deleted;
+	}
+	
+//	ゲッターセッター
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getNickname() {		
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
 }
--- src/main/java/com/example/demo/entity/Status.java
+++ src/main/java/com/example/demo/entity/Status.java
@@ -3,6 +3,7 @@ package com.example.demo.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -11,9 +12,8 @@ import lombok.Data;
 @Table(name = "status")
 @Data
 public class Status {
-
 	@Id
-	@GeneratedValue(strategy = GenrationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "status_id")
 	private Integer statusId;
 
