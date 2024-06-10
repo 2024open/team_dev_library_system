c1944360d11f8b8844f5369aec521c40f3ea04e8
student <student@AQ2-0673>
2024-06-10 10:39:19 +0900

    lendItem()追加/藤上

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -1,8 +1,15 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminController {
+
+	@GetMapping("/admin/lenditems")
+	public String lendItem() {
+		//		処理は後で書く
+		return "";
+	}
 
 }
