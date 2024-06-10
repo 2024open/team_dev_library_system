77bdc8a633a09f472bec3818830895dd7909c611
student <student@AQ2-0673>
2024-06-10 10:39:05 +0900

    home()追加/青木

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -1,8 +1,14 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminController {
 
+	@GetMapping("/admin/home")
+	public String home() {
+//		処理は後で書く
+		return "";
+	}
 }
