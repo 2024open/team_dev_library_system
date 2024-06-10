fc84d907b5ef2195c6f1ebce7d7da03869cb1616
student <student@AQ2-0673>
2024-06-10 15:09:18 +0900

    ログイン画面

--- src/main/java/com/example/demo/entity/Notice.java
+++ src/main/java/com/example/demo/entity/Notice.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -15,7 +16,7 @@ import lombok.Data;
 public class Notice {
 
 	@Id
-	@GeneratedValue(strategy = GenrationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "notice_id")
 	private Integer noticeId;
 
--- src/main/java/com/example/demo/entity/Status.java
+++ src/main/java/com/example/demo/entity/Status.java
@@ -3,6 +3,7 @@ package com.example.demo.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -13,7 +14,7 @@ import lombok.Data;
 public class Status {
 
 	@Id
-	@GeneratedValue(strategy = GenrationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "status_id")
 	private Integer statusId;
 
