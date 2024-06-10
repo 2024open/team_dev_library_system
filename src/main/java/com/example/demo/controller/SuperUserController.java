34082c14c02c0896358f705e96abdd88721850b8
student <student@AQ2-0673>
2024-06-10 09:44:06 +0900

    urlにhome追加

--- src/main/java/com/example/demo/controller/SuperUserController.java
+++ src/main/java/com/example/demo/controller/SuperUserController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SuperUserController {
-	
-	@GetMapping("/")
+
+	@GetMapping({ "/", "/home" })
 	public String index() {
 		return "testFile";
 	}
