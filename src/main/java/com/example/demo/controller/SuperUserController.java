4144212d8e1b3388d8944493990127ef32f497fd
Keisuke-Nakano-La <437-student008@la-study.com>
2024-06-11 13:23:56 +0900

    コントローラー修正

--- src/main/java/com/example/demo/controller/SuperUserController.java
+++ src/main/java/com/example/demo/controller/SuperUserController.java
@@ -1,9 +1,7 @@
 package com.example.demo.controller;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,81 +11,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.entity.Account;
-import com.example.demo.entity.AllUserLendList;
-import com.example.demo.entity.Genre;
-import com.example.demo.entity.Status;
-import com.example.demo.repository.ALLUserLendListRepoitory;
 import com.example.demo.repository.AccountRepository;
-import com.example.demo.repository.GenreRepository;
-import com.example.demo.repository.StatusRepository;
 
 import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class SuperUserController {
 
-	@Autowired
-	ALLUserLendListRepoitory allUserLendListrepository;
-
-	@Autowired
-	HttpSession session;
 
 	@Autowired
 	AccountRepository accountRepository;
 	
 	@Autowired
-	GenreRepository genreRepository;
-	
-	@Autowired
-	StatusRepository statusRepository;
+	HttpSession session;
 
-	//貸出物一覧表示
-	@GetMapping({ "/", "/home" })
-	public String index(
-			@RequestParam(value = "category", defaultValue = "") Integer category,
-			Model model) {
-		List<AllUserLendList> lendItemList = null;
-
-		Map<Integer, String> genreMap = new HashMap<>();
-
-		List<Genre> genreMapList = genreRepository.findAll();
-
-		//Mapで格納
-		for (Genre genre : genreMapList) {
-
-			genreMap.put(genre.getGenreId(), genre.getGenreName());
-
-		}
-		
-		Map<Integer, String> statusMap = new HashMap<>();
-
-		List<Status> statusMapList = statusRepository.findAll();
-
-		//Mapで格納
-		for (Status status : statusMapList) {
-
-			statusMap.put(status.getStatusId(), status.getStatusName());
-
-		}
-
-		model.addAttribute("genreMap", genreMap);
-		model.addAttribute("statusMap", statusMap);
-
-		if (category == null) {
-			lendItemList = allUserLendListrepository.sqlALLUserLendJoin();
-		} else if (category == 1) {
-			lendItemList = allUserLendListrepository.sqlALLUserBookLendJoin();
-		} else if (category == 2) {
-			lendItemList = allUserLendListrepository.sqlALLUserCDLendJoin();
-		} else if (category == 3) {
-			lendItemList = allUserLendListrepository.sqlALLUserDVDLendJoin();
-		} else if (category == 4) {
-			lendItemList = allUserLendListrepository.sqlALLUserKamishibaiLendJoin();
-		}
-		model.addAttribute("lendItemList", lendItemList);
-
-		return "index";
-	}
 
 	//	管理者ログイン画面表示
 	@GetMapping({ "/su/login", "/su/logout" })
