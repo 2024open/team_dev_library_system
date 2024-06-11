5c600ca9e4ebf77d779f62825dffe0ddd4543222
student <student@AQ2-0673>
2024-06-11 18:06:28 +0900

    マージ用2

--- src/main/java/com/example/demo/controller/LibrarianController.java
+++ src/main/java/com/example/demo/controller/LibrarianController.java
@@ -11,6 +11,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.demo.entity.Notice;
+import com.example.demo.model.SuperUser;
+import com.example.demo.repository.NoticeRepository;
+
+import jakarta.servlet.http.HttpSession;
+
 import com.example.demo.entity.Account;
 import com.example.demo.entity.AdminLendList;
 import com.example.demo.entity.AdminLendRoom;
@@ -32,13 +38,25 @@ public class LibrarianController {
 	LibrarianService librarianService;
 
 	@Autowired
+	CategoryRepository categoryRepository;
+
+	@Autowired
+	AdminLendRoomRepository adminLendRoomRepository;
+
+	@Autowired
+	HttpSession session;
+
+	@Autowired
+	SuperUser superUser;
+
+	@Autowired
 	AccountRepository accountRepository;
 
 	@Autowired
 	LendItemRepository lendItemRepository;
 
 	@Autowired
-	CategoryRepository categoryRepository;
+	NoticeRepository noticeRepository;
 
 	@Autowired
 	LendingItemRepository lendingItemRepository;
@@ -46,8 +64,79 @@ public class LibrarianController {
 	@Autowired
 	AdminLendListRepoitory adminLendListRepository;
 
-	@Autowired
-	AdminLendRoomRepository adminLendRoomRepository;
+	// お知らせ一覧表示
+	@GetMapping("/librarian/notice")
+	public String notice(Model model) {
+
+		List<Notice> noticeList = noticeRepository.findAll();
+
+		model.addAttribute("notices", noticeList);
+		return "noticeAdmin";
+	}
+
+	// お知らせ新規登録画面の表示
+	@GetMapping("/notice/add")
+	public String create() {
+		return "addNotice";
+	}
+
+	// 新規登録処理
+	@PostMapping("/notice/add")
+	public String store(
+			@RequestParam(value = "title", defaultValue = "") String title,
+			@RequestParam(value = "content", defaultValue = "") String content,
+			Model model) {
+
+		Integer userId = superUser.getLibraryId();
+		Integer libraryId = superUser.getUserId();
+
+		// Noticeオブジェクトの生成
+		Notice notice = new Notice(libraryId, userId, title, content);
+		// noticeテーブルへの反映（INSERT）
+
+		noticeRepository.save(notice);
+		return "redirect:/librarian/notice";
+	}
+
+	// お知らせ更新画面表示
+	@GetMapping("/notice/{noticeId}/edit")
+	public String edit(
+			@PathVariable("noticeId") Integer noticeId,
+			Model model) {
+
+		// itemsテーブルをID（主キー）で検索
+		Notice notice = noticeRepository.findById(noticeId).get();
+		model.addAttribute("notice", notice);
+		return "editNotice";
+	}
+
+	// お知らせ更新処理
+	@PostMapping("/notice/{noticeId}/edit")
+	public String update(
+			@PathVariable("noticeId") Integer noticeId,
+			@RequestParam(value = "title", defaultValue = "") String title,
+			@RequestParam(value = "content", defaultValue = "") String content,
+			Model model) {
+
+		Integer userId = superUser.getLibraryId();
+		Integer libraryId = superUser.getUserId();
+		Notice notice = new Notice(noticeId, libraryId, userId, title, content);
+
+		noticeRepository.save(notice);
+
+		return "redirect:/librarian/notice";
+	}
+
+	// お知らせ削除処理
+	@PostMapping("/items/{noticeId}/delete")
+	public String delete(@PathVariable("noticeId") Integer noticeId, Model model) {
+
+		noticeRepository.deleteById(noticeId);
+
+		return "redirect:/librarian/notice";
+	}
+
+	}
 
 	//貸出物一覧表示
 	@GetMapping("/librarian/lenditems")
